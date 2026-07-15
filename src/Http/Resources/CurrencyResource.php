30719645806e4c18a4dfa61e3af75b44fb986498
Molitor István <istvanmolitor@gmail.com>
2026-07-15 21:10:25 +0200

    currency resource

--- src/Http/Resources/CurrencyResource.php
+++ src/Http/Resources/CurrencyResource.php
@@ -6,25 +6,6 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use OpenApi\Attributes as OA;
 
-#[OA\Schema(
-    schema: 'Currency',
-    title: 'Currency',
-    description: 'Currency information',
-    properties: [
-        new OA\Property(property: 'id', type: 'integer', example: 1),
-        new OA\Property(property: 'code', type: 'string', example: 'USD'),
-        new OA\Property(property: 'name', type: 'string', example: 'US Dollar'),
-        new OA\Property(property: 'symbol', type: 'string', example: '$'),
-        new OA\Property(property: 'decimals', type: 'integer', example: 2),
-        new OA\Property(property: 'decimal_separator', type: 'string', example: '.'),
-        new OA\Property(property: 'thousands_separator', type: 'string', example: ','),
-        new OA\Property(property: 'is_symbol_first', type: 'boolean', example: true),
-        new OA\Property(property: 'is_enabled', type: 'boolean', example: true),
-        new OA\Property(property: 'is_default', type: 'boolean', example: false),
-        new OA\Property(property: 'created_at', type: 'string', format: 'date-time'),
-        new OA\Property(property: 'updated_at', type: 'string', format: 'date-time'),
-    ]
-)]
 class CurrencyResource extends JsonResource
 {
     /**
